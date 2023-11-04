c813a2808151dd5f292b94d1be8b17c17950fb1b
Aastha <Aastha@macbook-air.home>
2023-11-04 01:14:29 -0400

    Initial commit

--- server.js
+++ server.js
@@ -2,35 +2,37 @@
  * * I declare that this assignment is my own work in accordance with Seneca's* 
  * Academic Integrity Policy:*
  * * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html*
- * * Name: RAZAN NAYEF______________________ Student ID: ___137809190___________ Date: _2023-09-28_____________*
+ * Name: Aastha Kalpeshkumar Patel      Student ID: 118841220    Date: 3 movember 2023   *
  * ********************************************************************************/
-
 
 const legoData = require("./modules/legoSets");
 const express = require("express");
 const app = express();
 const path = require("path");
+app.set("view engine","ejs");
 
 const { initialize, getAllSets, getSetByNum, getSetsByTheme } = legoData;
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/home.html"));
+  //res.sendFile(path.join(__dirname, "views/home.html"));
+  res.render("home");
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/about.html"));
+  //res.sendFile(path.join(__dirname, "views/about.html"));
+  res.render("about");
 });
 
 app.get("/lego/sets", (req, res) => {
   const theme = req.query.theme;
   if (theme) {
     getSetsByTheme(theme)
-      .then((data) => res.send(data))
+      .then((data) => res.render("sets", { sets: data }))
       .catch((err) => res.status(404).send(err));
   } else {
     getAllSets()
-      .then((data) => res.send(data))
+      .then((data) => res.render("sets", { sets: data }))
       .catch((err) => res.status(404).send(err));
   }
 });
@@ -39,12 +41,13 @@ app.get("/lego/sets/:setNum", (req, res) => {
   const num = req.params.setNum;
 
   getSetByNum(num)
-    .then((data) => res.send(data))
+    .then((data) => res.render("sets", { sets: data }))
     .catch((err) => res.status(404).send(err));
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views/404.html"));
+  //res.status(404).sendFile(path.join(__dirname, "views/404.html"));
+  res.status(404).render("404");
 });
 
 initialize()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./views/*.html`], // all .html files
+  content: [`./views/**/*.ejs`], // all .html files
   theme: {
     extend: {},
   },
   daisyui: {
-    themes: ['valentine'],
+    themes: ['bumblebee'],
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')]
 }
+
 
 
